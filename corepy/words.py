01956c0ef885012a8ea0790ef511ff12bc1fefa3
Pankaj Arora <pankajarora19@gmail.com>
2020-05-04 21:12:29 +0530

    Added command line argument functionality

--- corepy/words.py
+++ corepy/words.py
@@ -1,7 +1,8 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    story = urlopen("http://sixty-north.com/c/t.txt")
+def fetch_words(url):
+    story = urlopen(url)
     story_words = []
     for line in story:
         line_words = line.decode('utf8').split()
@@ -10,13 +11,13 @@ def fetch_words():
     story.close()
     return story_words
 
-def print_words(story_words):
-    for words in story_words:
-        print(words)
+def print_items(items):
+    for item in items:
+        print(item)
 
-def main():
-    words = fetch_words()
-    print_words(words)
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
         
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])

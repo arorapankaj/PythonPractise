96232901b333f630ece841ba277e2962a8ca9311
Pankaj Arora <pankajarora19@gmail.com>
2020-05-04 22:54:07 +0530

    Added Docstrings for module and its functions

--- corepy/words.py
+++ corepy/words.py
@@ -1,7 +1,22 @@
+"""Retrive and prints words from a URL.
+
+Usage:
+    
+    python3 words.py <URL>
+"""
 import sys
 from urllib.request import urlopen
 
 def fetch_words(url):
+    """Fetch a list of words from a URL.
+    
+    Args:
+        url: The URL of a UTF-8 text document.
+        
+    Returns:
+        A list of strings containing the words from
+        the document.
+    """
     story = urlopen(url)
     story_words = []
     for line in story:
@@ -11,13 +26,26 @@ def fetch_words(url):
     story.close()
     return story_words
 
+    
 def print_items(items):
+    """Prints items one per line.
+    
+    Args:
+        An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
+
 def main(url):
+    """Prints each word from a text document mentioned in the url.
+    
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
-        
+
+    
 if __name__ == "__main__":
     main(sys.argv[1])

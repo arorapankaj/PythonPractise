5d49be0b516215eb60b7169e00c60f26c02e84f2
Pankaj Arora <pankajarora19@gmail.com>
2020-05-04 20:54:23 +0530

    Added dunder name logic

--- corepy/words.py
+++ corepy/words.py
@@ -1,12 +1,16 @@
 from urllib.request import urlopen
 
-story = urlopen("http://sixty-north.com/c/t.txt")
-story_words = []
-for line in story:
-    line_words = line.decode('utf8').split()
-    for words in line_words:
-        story_words.append(words)
-story.close()
+def fetch_words():
+    story = urlopen("http://sixty-north.com/c/t.txt")
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf8').split()
+        for words in line_words:
+            story_words.append(words)
+    story.close()
 
-for words in story_words:
-    print(words)
+    for words in story_words:
+        print(words)
+        
+if __name__ == "__main__":
+    fetch_words()

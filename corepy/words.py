21d3c8d2d2981a4806214bd96f69fadbcd422a91
Pankaj Arora <pankajarora19@gmail.com>
2020-05-04 21:06:39 +0530

    Seperated printing of words and main function

--- corepy/words.py
+++ corepy/words.py
@@ -8,9 +8,15 @@ def fetch_words():
         for words in line_words:
             story_words.append(words)
     story.close()
+    return story_words
 
+def print_words(story_words):
     for words in story_words:
         print(words)
+
+def main():
+    words = fetch_words()
+    print_words(words)
         
 if __name__ == "__main__":
-    fetch_words()
+    main()

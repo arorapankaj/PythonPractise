3bd6817a50949bd40c8f67f63b886616c6f3673c
Pankaj Arora <pankajarora19@gmail.com>
2020-05-04 20:50:29 +0530

    Code to import words from a URL

---
+++ corepy/words.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+
+story = urlopen("http://sixty-north.com/c/t.txt")
+story_words = []
+for line in story:
+    line_words = line.decode('utf8').split()
+    for words in line_words:
+        story_words.append(words)
+story.close()
+
+for words in story_words:
+    print(words)
